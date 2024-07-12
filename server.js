fc2ea5954236a0ab60b81ae72dbc12c4143ce018
Fallou Fall <ffall@gravitasgroup.io>
2024-07-12 19:26:05 +0000

    Added hello world endpoint

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Hello World !");
+
+const { createServer } = require('node:http');
+
+const  url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    if (req.method === 'GET' && parsedUrl.path === '/hello') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World !');
+    }
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
