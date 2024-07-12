d8c6a66144b6f02ad36a3b62ffdb3c9885592cb6
Amane <sasouza907@gmail.com>
2024-07-12 20:03:55 +0000

    ajout server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const { createServer } = require('node:http');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    const parseUrl=url.parse(req.url, true);
-    if (req.method === 'GET' && parseUrl.path === '/hello'){
+    const parseUrl = url.parse(req.url, true);
+    if (req.method === 'GET' && parseUrl.pathname === '/hello'){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Hello World');
+    } else {
+        res.statusCode = 404;
+        res.end('Not Found');
     }
-
 });
 
 server.listen(port, hostname, () => {
