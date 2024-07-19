3f5402aa528b7e4025848847f03a7b106f313266
DIARRA <boubs408@gmail.com>
2024-07-19 01:10:14 +0000

    Added hello world in server.js

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const { createServer } = require('node:http');
-const url=require('url');
+
+const  url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    const parseUrl=url.parse(req.url,true);
-    if(req.method=='GET'&& parseUrl.pathname==='/hello' ){
-        res.statusCode=200;
-        res.setHeader('Content-Type','text/plain');
-        res.end('hello world');
+    const parsedUrl = url.parse(req.url, true);
+    res.setHeader('Content-Type', 'text/plain');
+    // Note: This is for debuging purpose
+    //console.log('parsedUrl: ' + JSON.stringify(parsedUrl))
+
+    if (req.method === 'GET' && parsedUrl.pathname === '/hello') {
+        let query = parsedUrl.query
+        if(query.name !== undefined) {
+            res.statusCode = 200; //Ok, @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+            res.end('Hello ' + query.name + ' !');
+        } else {
+            res.statusCode = 400; // Bad request
+            res.end("Please set the parameter 'name' ")
+        }
+    } else {
+        res.statusCode = 404 // Not found
+        res.end('Not found')
     }
-    else{
-        res.statusCode=404;
-        res.end('Not Found');
-    }
-    
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
 });
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
